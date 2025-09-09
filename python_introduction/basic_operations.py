3839a464251313ac0721e9a5821665703960ce01
adebisi_olayinka <adebisio024@gmail.com>
2025-09-10 00:58:20 +0100

    Basic arithemtic 0

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = int(10)
+number2 = int(5)
+# performing basic arithmetic operations
+sum = number1 + number2
+difference = number1 - number2
+product = number2*number1
+print("Addition of", number1, "and" ,number2, "is", sum)
+print(f"Subtraction of {number1} and {number2} is {difference}")
+print(f"Multiplication of {number1} and {number2} is {product}")
