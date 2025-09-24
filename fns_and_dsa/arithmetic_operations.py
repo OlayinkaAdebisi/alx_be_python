a39672ca1418911a9f80c7147c94e556ccbda590
adebisi_olayinka <adebisio024@gmail.com>
2025-09-24 17:26:16 +0100

    0. Arithmetic Operations Function

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,18 @@
+#stores the operation signs in a Tuple
+operation = ("add","subtract","multiply","divide")
+def perform_operation(num1: float,num2: float,operation: str(operation)):
+#use the match statement to correctly assign the operator
+    match operation:
+        case "add":
+            return num1+num2
+        case "subtract":
+            return num1-num2
+        case "multiply":
+            return num1*num2
+        case "divide":
+            if num2==0:
+                print("Error division by zero")
+            else:
+                return num1/num2
+        case None:
+            print("Input correct operator: 'add','subtract','multiply','divide'")
