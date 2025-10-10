95086a72b4d838db06abfb2251800d71cb6cb5d1
adebisi_olayinka <adebisio024@gmail.com>
2025-10-10 19:36:27 +0100

    1. Mastering Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -23,8 +23,8 @@ class PrintBook(Book):
     def __str__(self):
          return f"PrintBook: {self.title} by {self.author}, Page count: {self.page_count}"
 class Library:
-    def __init__(self,books = []):
-        self.books = books
+    def __init__(self,books):
+        self.books = []
     def add_book(self, book):
         self.books.append(book)
     def list_books(self):
