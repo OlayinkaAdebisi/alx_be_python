c895ce7512077c73d70bf9f117d875ae4d6ae2f5
adebisi_olayinka <adebisio024@gmail.com>
2025-10-11 17:48:37 +0100

    3. Implementing Basic OOP for a Library Management System

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -3,7 +3,7 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = None
+        self._is_checked_out = False
 class Library:
     def __init__(self):
         self._books = []
