191e5f99ee32c73e2e86e34074302d3354fb2cc4
adebisi_olayinka <adebisio024@gmail.com>
2025-10-11 17:44:12 +0100

    3. Implementing Basic OOP for a Library Management System

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,24 +1,22 @@
+"""Implement a Library class with a private list _books to store instances of Book. Include methods to add_book, check_out_book(title), return_book(title), and list_available_books"""
 class Book:
-    def __init__(self,title,author):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
-        self._is_checked_out = False
-
+        self._is_checked_out = None
 class Library:
     def __init__(self):
-        self._books=[]
-    def add_book(self, book):
-        self._books.append(book)
-        print(self._books)
-    def check_out_book(self, title):
+        self._books = []
+    def add_book(self,Book):
+        self._books.append(Book)
+    def check_out_book(self,title):
+        for book_title in self._books:
+            if book_title.title == title:
+                book_title._is_checked_out = True
+    def return_book(self, title):
         for book in self._books:
-            if book.title.lower() == title.lower():
-                self._books.remove(book.title)
-                return
-        # if not found or already checked out
-        print(f"Book '{title}' is not available for checkout.")
-    def return_book(self,title):
-        add_book(title)
+            if book.title == title:
+                book._is_checked_out = False
     def list_available_books(self):
-        for i in self._books:
-            print(f"{self._books}")
+        for book in self._books:
+            print(f"{book.title} by {book.author}")
